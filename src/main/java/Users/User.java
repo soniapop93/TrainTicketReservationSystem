5ad3a2dcfbeaa1251ed7eaccb8c27f857e39f732
soniapop93 <popsonia85@gmail.com>
2023-10-03 19:00:44 +0300

    first commit

---
+++ src/main/java/Users/User.java
@@ -0,0 +1,27 @@
+package Users;
+
+public class User
+{
+    private int userId;
+    private String userFirstName;
+
+    public int getUserId()
+    {
+        return userId;
+    }
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getFirstName()
+    {
+        return userFirstName;
+    }
+
+    public void setFirstName(String userFirstName)
+    {
+        this.userFirstName = userFirstName;
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // TrainTicketReservationSystem
+        /*
+        Requirements:
+        [ ] - show trains available
+		[ ] - user should select the train which he wants to make a reservation for
+		[ ] - should select the number of seats (show also how many seats are available), the date for reservation and the location
+        [ ] - user should login
+        [ ] - if user doens't have an account, should create one
+		[ ] - reserve seat and send confirmation of the reservation through mail
+		[ ] - user can logout
+         */
+
+
+    }
+}
