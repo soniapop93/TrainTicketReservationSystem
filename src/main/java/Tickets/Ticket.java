85115f406edf1dc9908e631cddaef56f6c8a4a4a
soniapop93 <popsonia85@gmail.com>
2023-10-03 22:47:52 +0300

    started to implement the user class and ticket class

---
+++ src/main/java/Tickets/Ticket.java
@@ -0,0 +1,9 @@
+package Tickets;
+
+public class Ticket
+{
+    private long ticketId;
+
+    public long getTicketId() { return ticketId; }
+    public void setTicketId(long ticketId) { this.ticketId = ticketId; }
+}
--- src/main/java/Users/User.java
+++ src/main/java/Users/User.java
@@ -1,9 +1,18 @@
 package Users;
 
+import Tickets.Ticket;
+
+import java.util.ArrayList;
+
 public class User
 {
     private int userId;
     private String userFirstName;
+    private String userLastName;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private ArrayList<Ticket> tickets;
 
     public int getUserId()
     {
@@ -14,14 +23,38 @@ public class User
         this.userId = userId;
     }
 
-    public String getFirstName()
+    public String getUserFirstName()
     {
         return userFirstName;
     }
-
-    public void setFirstName(String userFirstName)
+    public void setUserFirstName(String userFirstName)
     {
         this.userFirstName = userFirstName;
     }
+
+    public String getUserLastName()
+    {
+        return userLastName;
+    }
+    public void setUserLastName(String userLastName)
+    {
+        this.userLastName = userLastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getPhoneNumber() {return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    
+
 }
 
