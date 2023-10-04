f4c510ffd3acb756b0b3b00f7d1cc8e4ef7871a7
soniapop93 <popsonia85@gmail.com>
2023-10-04 23:13:06 +0300

    implemented switch condition and message output for menu options

--- src/main/java/Logic/LogicTrainTicketReservationSystem.java
+++ src/main/java/Logic/LogicTrainTicketReservationSystem.java
@@ -1,20 +1,90 @@
 package Logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LogicTrainTicketReservationSystem {
+
+    private final ArrayList<String> optionList = new ArrayList<String>(Arrays.asList("See available train list", "Make a reservation", "Create new account", "Login in my account", "Logout", "Exit"));
+    private Scanner input = new Scanner(System.in);
+    private boolean loggedIn = false;
+
     public void reservationSystem() {
+        String optionStrSelected = ">>> You have selected option: ";
+
         printWelcomeMessage();
+
+        while (true) {
+            optionMenu(loggedIn);
+            System.out.print(">>> Add option number: ");
+            String input = getInputFromUser();
+            System.out.println("-----------------------");
+
+            if (loggedIn) {
+                switch (input) {
+                    default:
+                        System.out.println("Option not available");
+                    case "1": // See available train list
+                        System.out.println(optionStrSelected + "1." + optionList.get(0) + "\n");
+                        break;
+                    case "2": // Make a reservation
+                        System.out.println(optionStrSelected + "2." + optionList.get(1) + "\n");
+                        break;
+                    case "3": // Logout
+                        System.out.println(optionStrSelected + "3." + optionList.get(4) + "\n");
+                        break;
+                    case "4": // Exit
+                        System.out.println(optionStrSelected + "4." + optionList.get(5) + "\n");
+                        return;
+                }
+            }
+            else {
+                switch (input) {
+                    default:
+                        System.out.println("Option not available");
+                    case "1": // See available train list
+                        System.out.println(optionStrSelected + "1." + optionList.get(0) + "\n");
+                        break;
+                    case "2": // Make a reservation
+                        System.out.println(optionStrSelected + "2." + optionList.get(1) + "\n");
+                        break;
+                    case "3": // Create new account
+                        System.out.println(optionStrSelected + "3." + optionList.get(2) + "\n");
+                        break;
+                    case "4": // Login in my account
+                        System.out.println(optionStrSelected + "4." + optionList.get(3) + "\n");
+                        break;
+                    case "5": // Exit
+                        return;
+                }
+            }
+        }
     }
 
     private String getInputFromUser() {
-        Scanner input = new Scanner(System.in);
         return input.nextLine();
     }
 
     private void printWelcomeMessage() {
-        System.out.println("=================================================");
-        System.out.println("Welcome to the Train Ticket Reservation System");
-        System.out.println("=================================================");
+        System.out.println("==================================================");
+        System.out.println("  Welcome to the Train Ticket Reservation System");
+        System.out.println("==================================================");
+        System.out.println(" ");
+    }
+
+    private void optionMenu(boolean loggedIn) {
+        String menuString = "";
+
+        System.out.println("Please select one of the options: ");
+
+        if (loggedIn) {
+            menuString = "1. " + optionList.get(0) + "\n2. " + optionList.get(1) + "\n3. " + optionList.get(4) + "\n4. " + optionList.get(5);
+        }
+        else {
+            menuString = "1. " + optionList.get(0) + "\n2. " + optionList.get(1) + "\n3. " + optionList.get(2) + "\n4. " + optionList.get(3) + "\n5. " + optionList.get(5);
+        }
+
+        System.out.println(menuString);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import Logic.LogicTrainTicketReservationSystem;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main
 {
     public static void main(String[] args)
@@ -13,7 +11,7 @@ public class Main
 		[ ] - user should select the train which he wants to make a reservation for
 		[ ] - should select the number of seats (show also how many seats are available), the date for reservation and the location
         [ ] - user should login
-        [ ] - if user doens't have an account, should create one
+        [ ] - if user doesn't have an account, should create one
 		[ ] - reserve seat and send confirmation of the reservation through mail
 		[ ] - user can logout
          */
